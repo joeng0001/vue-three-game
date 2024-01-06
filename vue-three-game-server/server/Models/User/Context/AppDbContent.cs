dddfa724b3406b05c4909e9ee3be394b6a9474aa
joeng0001 <102034869+joeng0001@users.noreply.github.com>
2024-01-06 18:49:54 +0800

    enable default creation

--- vue-three-game-server/server/Models/User/Context/AppDbContent.cs
+++ vue-three-game-server/server/Models/User/Context/AppDbContent.cs
@@ -2,16 +2,22 @@
 using server.Model;
 public class UserContext : DbContext
 {
-    public UserContext(DbContextOptions<UserContext> options) : base(options)
+    public DbSet<User> Users { get; set; }
+    public  UserContext(DbContextOptions<UserContext> options) : base(options)
     {
         if (!Database.CanConnect())
         {
             Database.EnsureCreated();
+            if (!Users.Any())
+            {
+                Users.Add(new User { Name = "John Doe", password=new byte[1024],salt=new byte[10] });
+                SaveChanges(); 
+            }
         }
-        // To be test...
-        //Database.Migrate();
+
+        Database.Migrate();
         
     }
 
-    public DbSet<User> Users { get; set; }
+    
 }
